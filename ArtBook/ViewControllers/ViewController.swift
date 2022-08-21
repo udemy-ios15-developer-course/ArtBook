b8b798b5ea5fb7bf1c38741477ffea66fea8b671
Chris Hand <cj.hand@gmail.com>
2022-08-20 22:32:45 -1000

    Add button so the user can add art

--- ArtBook/ViewControllers/ViewController.swift
+++ ArtBook/ViewControllers/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonClicked))
+    }
+    
+    @objc
+    private func addButtonClicked() {
+        performSegue(withIdentifier: "toAddArt", sender: nil)
     }
 }
 

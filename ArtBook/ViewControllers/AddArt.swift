86754aec601df6f71225ad8186f950f1fe8b2fd4
Chris Hand <cj.hand@gmail.com>
2022-08-24 19:01:01 -1000

    Add a gesture recognizer to hide the keyboard

--- ArtBook/ViewControllers/AddArt.swift
+++ ArtBook/ViewControllers/AddArt.swift
@@ -13,10 +13,20 @@ class AddArt: UIViewController {
     @IBOutlet weak var artName: UITextField!
     @IBOutlet weak var artist: UITextField!
     @IBOutlet weak var artYear: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc
+    func hideKeyboard() {
+        view.endEditing(true)
     }
     
     @IBAction func saveClicked(_ sender: Any) {
     }
+    
 }

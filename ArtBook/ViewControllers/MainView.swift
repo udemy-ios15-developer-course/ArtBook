60185901a86c22c2441542a101ff49f47e9ccaee
Chris Hand <cj.hand@gmail.com>
2022-08-25 22:46:09 -0700

    Basics to get the table view working

--- ArtBook/ViewControllers/MainView.swift
+++ ArtBook/ViewControllers/MainView.swift
@@ -7,18 +7,31 @@
 
 import UIKit
 
-class MainView: UIViewController {
+class MainView: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var artList: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonClicked))
+        artList.dataSource = self
+        artList.delegate = self
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
     
     @objc
     private func addButtonClicked() {
         performSegue(withIdentifier: "toAddArt", sender: nil)
     }
 }
-

192f444b106a16c2ad389ea2f9842ae0533c05cf
Chris Hand <cj.hand@gmail.com>
2022-08-27 19:29:38 -0700

    Retrieve an individual item for the detail view

--- ArtBook/ViewControllers/ArtDetails.swift
+++ ArtBook/ViewControllers/ArtDetails.swift
@@ -24,7 +24,42 @@ class ArtDetails: UIViewController {
             return
         }
         
-        let id = selectedPaintingId!.uuidString
+        let id = selectedPaintingId?.uuidString
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
+        fetchRequest.predicate = NSPredicate(format: "id=%@", id!)
         
+        var results : [Any]
+        
+        do {
+            results = try context.fetch(fetchRequest)
+            if results.count == 0 {
+                return
+            }
+        } catch {
+            fatalError("Failed to get the requested item")
+        }
+        let resultSet = results as! [NSManagedObject]
+        let result = resultSet[0]
+        
+        if let name = result.value(forKey: "name") as? String {
+            artName = name
+        }
+        
+        if let artist = result.value(forKey: "artist") as? String {
+            self.artist.text = artist
+        }
+        
+        if let year = result.value(forKey: "year") as? Int {
+            artYear.text = String(year)
+        }
+        
+        if let imageData = result.value(forKey: "image") as? Data {
+            let image = UIImage(data: imageData)
+            art.image = image
+        }
     }
 }
+
